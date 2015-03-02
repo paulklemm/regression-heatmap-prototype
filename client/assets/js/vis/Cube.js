0bd9990539ee2a021d11e4de28f7435c4d054153
Paul Klemm <paul.klemm@googlemail.com>
2015-03-02 11:15:23 +0100

    Added Cube js file

---
+++ client/assets/js/vis/Cube.js
@@ -0,0 +1,6 @@
+RCUBE.Cube = function(canvasID, rSquared, names) {
+  this._canvasID = canvasID;
+  this.main(canvasID, this._data);
+};
+
+RCUBE.Heatmap.prototype.main = function (canvasID, heatmapData){ };
