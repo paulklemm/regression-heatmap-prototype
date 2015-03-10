ab0ce73a19315ec326fb3570a4344e363fba0410
Paul Klemm <paul.klemm@googlemail.com>
2015-03-10 13:52:30 +0100

    mouse events properly handled

--- client/app/directives/mouseevents.directive.js
+++ client/app/directives/mouseevents.directive.js
@@ -4,22 +4,45 @@ angular.module('cube')
     return {
       link: function(scope) {
         angular.element($window).on('mousedown', function(e) {
-          // Namespacing events with name of directive + event to avoid collisions
           scope.$broadcast('mouseevents::mousedown', e);
+        });
+      }
+    };
+  })
+  .directive('mousemove', function($window) {
+    return {
+      link: function(scope) {
+        angular.element($window).on('mousemove', function(e) {
+          scope.$broadcast('mouseevents::mousemove', e);
+        });
+      }
+    };
+  })
+  .directive('mouseup', function($window) {
+    return {
+      link: function(scope) {
+        angular.element($window).on('mouseup', function(e) {
+          scope.$broadcast('mouseevents::mouseup', e);
         });
       }
     };
   });
 
 angular.module('cube')
-  .directive('handleMouseEvents', function() {
+  .directive('handleSliceMouseEvents', function() {
     return {
       link: function($scope, element) {
+        $scope.mousedown = false;
         $scope.$on('mouseevents::mousedown', function(event, mouseDownEvent) {
-          // doSomethingFancy(element);
-          console.log("Catching Mouse Down");
-          console.log(mouseDownEvent);
-          console.log(element);
+          $scope.mousedown = true;
+        });
+        $scope.$on('mouseevents::mousemove', function(event, mouseDownEvent) {
+          if ($scope.mousedown) {
+            console.log("Mousedown");
+          }
+        });
+        $scope.$on('mouseevents::mouseup', function(event, mouseDownEvent) {
+          $scope.mousedown = false;
         });
       }
     };
